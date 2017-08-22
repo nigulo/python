188c40bcf98e21c7af956ef28af1a697b38ad2b3
Olspert Nigul <olspern1@cs-102.org.aalto.fi>
2017-08-22 11:14:30 +0300

    Changes and fixes

---
+++ MountWilson/remove_rotation/run.sh
@@ -0,0 +1,11 @@
+sbatch job.sh 10 0
+sbatch job.sh 10 1
+sbatch job.sh 10 2
+sbatch job.sh 10 3
+sbatch job.sh 10 4
+sbatch job.sh 10 5
+sbatch job.sh 10 6
+sbatch job.sh 10 7
+sbatch job.sh 10 8
+sbatch job.sh 10 9
+
