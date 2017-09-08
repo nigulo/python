2e7f68f55283963f873c7e8b6261a58b3df9b0e9
Olspert Nigul <olspern1@cs-102.org.aalto.fi>
2017-09-08 10:41:36 +0300

    Added residue calculation to postprocessing scripts

--- MountWilson/GP_quasiperiodic/process_GPR_results2.py
+++ MountWilson/GP_quasiperiodic/process_GPR_results2.py
@@ -15,10 +15,17 @@ import os
 import mw_utils
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
+import GPR_QP
 
 num_iters = 300
 freq_row = 2
 n_eff_col = 9
+
+data_dir = "../GP_input"
+if data_dir == "../cleaned":
+    skiprows = 1
+else:
+    skiprows = 0
 
 
 try:
@@ -66,12 +73,13 @@ for [star, index, validity, cyc, cyc_se, cyc_std, length_scale, length_scale_se,
 
     if delta_bic < 6:
         continue
-    print type(index)
     file_name = "results/" + star + "_" + str(index) + "_results.txt"
     if not os.path.isfile(file_name) and index == 0:
         file_name = "results/" + star + "_results.txt"
     print "Loading " + star + " " + str(index)
     n_eff = -1
+    sig_var = -1
+    m = -1
     with open(file_name, "r") as ins:
         i = 0
         for line in ins:
@@ -86,8 +94,14 @@ for [star, index, validity, cyc, cyc_se, cyc_std, length_scale, length_scale_se,
             var = var.replace(' ', '')
             if var == "freq":
                 n_eff = float(fields[n_eff_col].strip())
-                break
+            if var == "sig_var":
+                sig_var = float(fields[1].strip())
+            if var == "m":
+                m = float(fields[1].strip())
+            
     assert(n_eff >= 0)
+    assert(sig_var >= 0)
+    assert(m >= 0)
     print n_eff
     if n_eff >= 5:#float(num_iters) / 10:
     #details = np.genfromtxt(file_name, usecols=(n_eff_col), skip_header=5, skip_footer=5)
@@ -100,6 +114,41 @@ for [star, index, validity, cyc, cyc_se, cyc_std, length_scale, length_scale_se,
     #if (details[freq_row]) >= float(num_iters) / 10:
         #cycles.append((cyc*365.25, std_2/2*365.25)) # one sigma
         output_cycles.write(star + " " + str(validity) + " " + str(cyc) + " " + str(cyc_std) + " " + str(delta_bic) + "\n")    
+    
+        dat = np.loadtxt(data_dir+"/"+star + ".dat", usecols=(0,1), skiprows=skiprows)
+        
+        offset = 1979.3452
+        
+        t_orig = dat[:,0]
+        y = dat[:,1]
+        
+        t = t_orig/365.25
+        t += offset
+        noise_var = mw_utils.get_seasonal_noise_var(t, y)
+        n = len(t)
+        
+        gpr_gp = GPR_QP.GPR_QP(sig_var=sig_var, length_scale=length_scale, freq=1.0/cyc, noise_var=noise_var, rot_freq=0, rot_amplitude=0, trend_var=trend_var, c=0.0)
+        t_test = np.linspace(min(t), max(t), 500)
+        (f_mean, pred_var, loglik) = gpr_gp.fit(t, y-m, t_test)
+        (f_t, _, _) = gpr_gp.fit(t, y-m, t)
+        f_mean += m
+        residue = y - (f_t + m)
+        
+        dat = np.column_stack((t_orig, residue))
+        np.savetxt("residues/" + star + ".dat", dat, fmt='%f')
+        fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
+        fig.set_size_inches(18, 12)
+    
+        ax1.plot(t, y, 'b+')
+        #ax2.plot(t, y_wo_rot, 'r+')
+        ax1.plot(t_test, f_mean, 'k-')
+        ax1.fill_between(t_test, f_mean + 2.0 * np.sqrt(pred_var), f_mean - 2.0 * np.sqrt(pred_var), alpha=0.1, facecolor='lightgray', interpolate=True)
+    
+        ax2.plot(t, residue, 'b+')
+  
+        fig.savefig("fits/"+star + '.png')
+        plt.close()
+    
     else:
         print "Omitting " + star + " " + str(index) + " due to too low n_eff"
 
