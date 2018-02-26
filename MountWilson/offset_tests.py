17b8aa9b607f7821de120e55833515f994fa1b11
nigulo <nigulolspert@hotmail.com>
2018-02-26 11:42:25 +0200

    changes

--- MountWilson/offset_tests.py
+++ MountWilson/offset_tests.py
@@ -105,8 +105,6 @@ max_emp_mean_err = 0.5
 num_bins = 10
 bin_locs = np.linspace(0.0, max_emp_mean_err, num_bins)
 
-
-
 fig_stats, ax_stats_1 = plt.subplots(nrows=1, ncols=1, sharex=True, sharey=False)
 fig_stats.set_size_inches(6, 4)
 #ax_stats_1.text(0.05, 0.9,'(b)', horizontalalignment='center', transform=ax_stats_1.transAxes, fontsize=panel_label_fs)
@@ -179,7 +177,9 @@ else:
         #print data
         #np.savetxt("cyclic.txt", data, fmt='%f')
         #noise_var_prop = mw_utils.get_seasonal_noise_var(t, y)
-        freqs = np.linspace(0.5*f, 2.0*f, 1000)
+        min_freq = 0.001
+        max_freq = 2.0*f
+        freqs = np.linspace(min_freq, max_freq, 1000)
 
         ls = LombScargle(t, y-np.mean(y), np.sqrt(noise_var), fit_mean=False)
         power_ls = ls.power(freqs, normalization='psd')
@@ -189,6 +189,10 @@ else:
         f_opt_ls = freqs[ls_local_maxima_inds][f_opt_ls_ind]
         ls_freqs[bin_index, counts[bin_index]] = f_opt_ls
 
+        min_power_ls = min(power_ls)
+        max_power_ls = max(power_ls)
+        norm_powers_ls = (power_ls - min_power_ls) / (max_power_ls - min_power_ls)
+
         gls = LombScargle(t, y, np.sqrt(noise_var))
         power_gls = gls.power(freqs, normalization='psd')
         
@@ -196,6 +200,10 @@ else:
         f_opt_gls_ind = np.argmax(power_gls[gls_local_maxima_inds])
         f_opt_gls = freqs[gls_local_maxima_inds][f_opt_gls_ind]
         gls_freqs[bin_index, counts[bin_index]] = f_opt_gls
+        
+        min_power_gls = min(power_gls)
+        max_power_gls = max(power_gls)
+        norm_powers_gls = (power_gls - min_power_gls) / (max_power_gls - min_power_gls)
         
         slope, intercept, r_value, p_value, std_err = stats.linregress(t, y)
         
@@ -213,21 +221,52 @@ else:
         f_opt_bglst_ind = np.argmax(probs[bglst_local_maxima_inds])
         f_opt_bglst = freqs[bglst_local_maxima_inds][f_opt_bglst_ind]
         bglst_freqs[bin_index, counts[bin_index]] = f_opt_bglst
+        
+        min_prob_bglst = min(probs)
+        max_prob_bglst = max(probs)
+        norm_probs_bglst = (probs - min_prob_bglst) / (max_prob_bglst - min_prob_bglst)
+        
         dats.append((t, y, w))
+        
+        fig1, (ax_a, ax_b) = plt.subplots(nrows=2, ncols=1, sharex=False, sharey=False)
+        fig1.set_size_inches(6, 7)
+    
+        ax_a.text(0.95, 0.9,'(a)', horizontalalignment='center', transform=ax_a.transAxes, fontsize=panel_label_fs)
+        ax_b.text(0.95, 0.9,'(b)', horizontalalignment='center', transform=ax_b.transAxes, fontsize=panel_label_fs)
         
         fig_tmp, ax_tmp = plt.subplots(nrows=1, ncols=1, sharex=True, sharey=False)
         fig_tmp.set_size_inches(12, 8)
-        ax_tmp.plot(t, y, "k+")
+        ax_a.plot(t, y, "k+")
         t_model = np.linspace(max_t, min_t, 200)
         y_model = gls.model(t_model, f)   
-        ax_tmp.plot(t_model, y_model, 'r-')
+        ax_a.plot(t_model, y_model, 'r-')
         y_model = gls.model(t_model, f_opt_gls)   
-        ax_tmp.plot(t_model, y_model, 'b:')
+        ax_a.plot(t_model, y_model, 'b:')
         y_model = ls.model(t_model, f_opt_ls)   
-        ax_tmp.plot(t_model, y_model+np.mean(y), 'g:')
+        ax_a.plot(t_model, y_model+np.mean(y), 'g:')
         (tau, mean, cov, y_model, loglik) = bglst.model(f_opt_bglst, t_model)   
-        ax_tmp.plot(t_model, y_model, 'k:')
-        fig_tmp.savefig("offset_tests/temp/" + str(bin_index) + "_" + str(counts[bin_index]) + ".png")
+        ax_a.plot(t_model, y_model, 'k:')
+        
+        ax_b.plot(freqs, norm_probs_bglst, 'r-')
+        ax_b.plot([f_opt_bglst, f_opt_bglst], [0, 1], 'r-')
+
+        ax_b.plot(freqs, norm_powers_gls, 'b--')
+        ax_b.plot([f_opt_gls, f_opt_gls], [0, 1], 'b--')
+
+        ax_b.plot(freqs, norm_powers_ls, 'g-.')
+        ax_b.plot([f_opt_ls, f_opt_ls], [0, 1], 'g-.')
+        
+        ax_b.plot([f, f], [0, 1], 'k:')
+        
+        ax_a.set_xlabel(r'$t$', fontsize=axis_label_fs)#,fontsize=20)
+        ax_a.set_ylabel(r'y', fontsize=axis_label_fs)#,fontsize=20)
+        ax_a.set_xlim([min(t), max(t)])
+        
+        ax_b.set_xlabel(r'$f$', fontsize=axis_label_fs)#,fontsize=20)
+        ax_b.set_ylabel(r'Power', fontsize=axis_label_fs)#,fontsize=20)
+        ax_b.set_xlim([min_freq, max_freq])
+        
+        fig1.savefig("offset_tests/temp/" + str(bin_index) + "_" + str(counts[bin_index]) + ".eps")
         
         print f, f_opt_ls, f_opt_gls, f_opt_bglst
         
@@ -249,24 +288,25 @@ ls_err_means = np.mean(ls_errs, axis=1)
 
 print gls_err_means, ls_err_means
 
-#for bs_index in np.arange(0, num_bs):
-#    bglst_errs_bs = np.random.choice(bglst_errs, size=len(bglst_errs))
-#    gls_errs_bs = np.random.choice(gls_errs, size=len(gls_errs))
-#    ls_errs_bs = np.random.choice(ls_errs, size=len(ls_errs))
-#    bglst_err_means_bs[exp_no, bs_index] = np.mean(bglst_errs_bs)
-#    gls_err_means_bs[exp_no, bs_index] = np.mean(gls_errs_bs)
-#    ls_errs_bs = np.random.choice(ls_errs, size=len(ls_errs))
+for bin_index in np.arange(0, num_bins):
+    for bs_index in np.arange(0, num_bs):
+        bglst_errs_bs = np.random.choice(bglst_errs[bin_index,:], size=np.size(bglst_errs)[1])
+        gls_errs_bs = np.random.choice(gls_errs[bin_index,:], size=np.size(gls_errs)[1])
+        ls_errs_bs = np.random.choice(ls_errs[bin_index,:], size=np.size(ls_errs)[1])
+        bglst_err_means_bs[bin_index, bs_index] = np.mean(bglst_errs_bs)
+        gls_err_means_bs[bin_index, bs_index] = np.mean(gls_errs_bs)
+        ls_err_means_bs[bin_index, bs_index] = np.mean(ls_errs_bs)
 
 ###############################################################################
 # Plot experiment statistics
 
-line1, = ax_stats_1.plot(bin_locs, bglst_err_means, 'r+', markerfacecolor='None', label = "BGLST")
-line2, = ax_stats_1.plot(bin_locs, gls_err_means, 'bx', markerfacecolor='None', label = "GLS")
-line3, = ax_stats_1.plot(bin_locs, ls_err_means, 'go', markerfacecolor='None', label = "LS")
+line1, = ax_stats_1.plot(bin_locs, bglst_err_means, 'r-', markerfacecolor='None', label = "BGLST")
+line2, = ax_stats_1.plot(bin_locs, gls_err_means, 'b--', markerfacecolor='None', label = "GLS")
+line3, = ax_stats_1.plot(bin_locs, ls_err_means, 'g-.', markerfacecolor='None', label = "LS")
 
-#ax_stats_1.fill_between(means, np.percentile(bglst_err_means_bs, 2.5, axis=1), np.percentile(bglst_err_means_bs, 97.5, axis=1), alpha=0.2, facecolor='lightsalmon', interpolate=True)
-#ax_stats_1.fill_between(means, np.percentile(gls_err_means_bs, 2.5, axis=1), np.percentile(gls_err_means_bs, 97.5, axis=1), alpha=0.2, facecolor='lightblue', interpolate=True)
-#ax_stats_1.fill_between(means, np.percentile(ls_err_means_bs, 2.5, axis=1), np.percentile(ls_err_means_bs, 97.5, axis=1), alpha=0.2, facecolor='lightgreen', interpolate=True)
+ax_stats_1.fill_between(bin_locs, np.percentile(bglst_err_means_bs, 2.5, axis=1), np.percentile(bglst_err_means_bs, 97.5, axis=1), alpha=0.2, facecolor='lightsalmon', interpolate=True)
+ax_stats_1.fill_between(bin_locs, np.percentile(gls_err_means_bs, 2.5, axis=1), np.percentile(gls_err_means_bs, 97.5, axis=1), alpha=0.2, facecolor='lightblue', interpolate=True)
+ax_stats_1.fill_between(bin_locs, np.percentile(ls_err_means_bs, 2.5, axis=1), np.percentile(ls_err_means_bs, 97.5, axis=1), alpha=0.2, facecolor='lightgreen', interpolate=True)
 
 ax_stats_1.set_position([0.12, 0.12, 0.85, 0.75])
 ax_stats_1.legend(handles=[line1, line2, line3], bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=3, mode="expand", borderaxespad=0., handletextpad=0., fontsize=panel_label_fs)#, columnspacing=10)
