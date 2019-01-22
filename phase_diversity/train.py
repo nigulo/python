b8b586da30ff018f049763726a04a2cce512f303
nigul <nigulolspert@gmail.com>
2019-01-22 14:03:56 +0100

    changes

--- phase_diversity/train.py
+++ phase_diversity/train.py
@@ -9,22 +9,37 @@ import matplotlib as mpl
 import matplotlib.colorbar as cb
 from matplotlib import cm
 
-nx = 114
-ny = 114
+nx = 320
+ny = 320
+
+zoomin_start = nx/2 - 33
+zoomin_end = ny/2 + 33
+
+psf_vals_nx = 66
+psf_vals_ny = 66
+
+
+aperture_func = lambda u: psf.aperture_circ(u, 0.2, 100.0)
 
 n_coefs = 10
-n_data = 100
+n_data = 3
 n_train = int(n_data*0.75)
 
 n_epochs = 500
+
+def reverse_colourmap(cmap, name = 'my_cmap_r'):
+     return mpl.colors.LinearSegmentedColormap(name, cm.revcmap(cmap._segmentdata))
 
 def create_model():
     
     coefs = keras.layers.Input((n_coefs,), name='coefs')
     #psf = keras.layers.Input(IMAGE_SHAPE, name='psf')
     
-    hidden1 = keras.layers.Dense(256, activation='relu')(coefs)
-    hidden2 = keras.layers.Reshape((16, 16, 1))(hidden1)
+    start_nx = 10#(((psf_vals_nx + 1)/2 + 1)/2 + 1)/2 + 2
+    start_ny = 10#(((psf_vals_ny + 1)/2 + 1)/2 + 1)/2 + 2
+    
+    hidden1 = keras.layers.Dense(start_nx*start_ny, activation='relu')(coefs)
+    hidden2 = keras.layers.Reshape((start_nx, start_ny, 1))(hidden1)
     hidden3 = keras.layers.Conv2D(18, (4, 4), activation='relu', padding='same')(hidden2)
     hidden4 = keras.layers.UpSampling2D((2, 2))(hidden3)
     hidden5 = keras.layers.Conv2D(8, (6, 6), activation='relu', padding='same')(hidden4)
@@ -66,16 +81,13 @@ def test(model):
         #    psf_vals[i] = psf.psf(ctf, nx, ny).get_incoh_vals()
     
         predicted_psfs = model.predict(coefs)
-        predicted_psfs = np.reshape(predicted_psfs, (n_test_data, nx, ny))
+        predicted_psfs = np.reshape(predicted_psfs, (n_test_data, psf_vals_nx, psf_vals_ny))
         
         extent=[0., 1., 0., 1.]
         plot_aspect=(extent[1]-extent[0])/(extent[3]-extent[2])#*2/3 
         
-        def reverse_colourmap(cmap, name = 'my_cmap_r'):
-             return mpl.colors.LinearSegmentedColormap(name, cm.revcmap(cmap._segmentdata))
-        
         #my_cmap = reverse_colourmap(plt.get_cmap('gnuplot'))
-        my_cmap = plt.get_cmap('winter')
+        my_cmap = reverse_colourmap(plt.get_cmap('binary'))#plt.get_cmap('winter')
         
         
         fig, axes = plt.subplots(nrows=n_test_data, ncols=2)
@@ -110,14 +122,14 @@ def test(model):
         fig.savefig('psf_nn.png')
         plt.close(fig)
 
-def gen_data(n_data, normalize=True, log=True):
+def gen_data(n_data, normalize=True, log=False):
     #coefs = np.zeros((n_data, n_coefs))
     coefs = np.random.normal(size=(n_data, n_coefs))
-    psf_vals = np.zeros((n_data, nx, ny))
+    psf_vals = np.zeros((n_data, psf_vals_nx, psf_vals_ny))
     for i in np.arange(0, n_data):
         pa = psf.phase_aberration(zip(np.arange(4, n_coefs + 4), coefs[i]))
-        ctf = psf.coh_trans_func(lambda u: 1.0, pa, lambda u: 0.0)
-        psf_vals[i] = psf.psf(ctf, nx, ny).get_incoh_vals()
+        ctf = psf.coh_trans_func(aperture_func, pa, lambda u: 0.0)
+        psf_vals[i] = psf.psf(ctf, nx, ny).get_incoh_vals()[zoomin_start:zoomin_end,zoomin_start:zoomin_end]
         if log:
             psf_vals[i] = np.log(psf_vals[i])
         if normalize:
@@ -129,7 +141,7 @@ def gen_data(n_data, normalize=True, log=True):
 
 print("Generating training data")
 coefs, psf_vals = gen_data(n_data)
-psf_vals = np.reshape(psf_vals, (len(psf_vals), nx, ny, 1))
+psf_vals = np.reshape(psf_vals, (len(psf_vals), psf_vals_nx, psf_vals_ny, 1))
 coefs_train = coefs[:n_train] 
 coefs_test = coefs[n_train:]
 psf_train = psf_vals[:n_train]
