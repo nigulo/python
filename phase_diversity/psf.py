cd502d8a5000f0ed1f8545040c2df44fd5bf657a
nigul <nigulolspert@gmail.com>
2019-01-22 14:30:00 +0100

    starting vectrorization

--- phase_diversity/psf.py
+++ phase_diversity/psf.py
@@ -28,23 +28,29 @@ class coh_trans_func():
         self.phase_aberr = phase_aberr
         self.phase_div = phase_div
        
-    def get_value(self, u):
+    def get_value(self, us):
         #if self.pupil_func(u) == 0:
         #    return 0.0
         #else:
         #    return np.exp(1.j * (self.phase_aberr.get_value(u) + self.phase_div(u)))
         #a = np.exp(1.j * (self.phase_aberr.get_value(u) + self.phase_div(u)))
         #b = np.cos(self.phase_aberr.get_value(u) + self.phase_div(u)) + 1.j * np.sin(self.phase_aberr.get_value(u) + self.phase_div(u))
-        return self.pupil_func(u)*np.exp(1.j * (self.phase_aberr.get_value(u) + self.phase_div(u)))
+        return self.pupil_func(us)*np.exp(1.j * (self.phase_aberr.get_value(us) + self.phase_div(us)))
 
 class psf():
     def __init__(self, coh_trans_func, nx, ny):
-        coh_vals = np.zeros((nx, ny))
-        for x in np.arange(0, nx):
-            for y in np.arange(0, ny):
-                norm_x = np.sqrt(2.0)*(float(x) - nx/2) / nx
-                norm_y = np.sqrt(2.0)*(float(y) - ny/2) / ny
-                coh_vals[x, y] = coh_trans_func.get_value(np.array([norm_x, norm_y]))
+        #coh_vals = np.zeros((nx, ny))
+        xs = np.linspace(-1.0, 1.0, nx)/np.sqrt(2)
+        ys = np.linspace(-1.0, 1.0, ny)/np.sqrt(2)
+        assert(len(xs) == nx)
+        assert(len(ys) == ny)
+        us = np.transpose([np.tile(xs, ny), np.repeat(ys, nx)])
+        #for x in np.arange(0, nx):
+        #    for y in np.arange(0, ny):
+        #        norm_x = np.sqrt(2.0)*(float(x) - nx/2) / nx
+        #        norm_y = np.sqrt(2.0)*(float(y) - ny/2) / ny
+        #        coh_vals[x, y] = coh_trans_func.get_value(np.array([norm_x, norm_y]))
+        coh_vals = coh_trans_func.get_value(us)
         vals = fft.fft2(coh_vals)
         vals = vals.real**2 + vals.imag**2
         #vals = fft.ifft2(vals)
@@ -57,12 +63,18 @@ class psf():
         return self.incoh_vals
 
 
-def aperture_circ(u, r=1.0, coef=5.0):
+def aperture_circ(us, r=1.0, coef=5.0):
+    scalar = False
+    if len(np.shape(us)) == 1:
+        scalar = True
+        us = np.array([us])
     if coef > 0.0:
-        return 0.5+0.5*special.erf(coef*(r-np.sqrt(np.sum(u**2))))
+        ret_val = 0.5+0.5*special.erf(coef*(r-np.sqrt(np.sum(us**2, axis=1))))
     else:
-        if np.sum(u**2) <= r*r: 
-            return 1.0
-        else: 
-            return 0.0
+        ret_val = np.zeros(np.shape(us)[0])
+        indices = np.where(np.sum(us**2, axis=1) <= r*r)[0]
+        ret_val[indices] = 1.0
+    if scalar:
+        ret_val = ret_val[0]
+    return ret_val
     
--- phase_diversity/psf_test.py
+++ phase_diversity/psf_test.py
@@ -5,19 +5,43 @@ import unittest
 class test_aperture_circ(unittest.TestCase):
 
     def test(self):
-        np.testing.assert_almost_equal(psf.aperture_circ(np.array([0.0, 0.0])), 1.0, 10)
-        np.testing.assert_almost_equal(psf.aperture_circ(np.array([0.0, 0.5])), 0.9997965239912775, 10)
-        np.testing.assert_almost_equal(psf.aperture_circ(np.array([0.0, 0.9])), 0.7602499389065231, 10)
-        np.testing.assert_almost_equal(psf.aperture_circ(np.array([0.0, 1.0])), 0.5, 10)
-        np.testing.assert_almost_equal(psf.aperture_circ(np.array([0.0, 1.1])), 0.23975006109347657, 10)
-        np.testing.assert_almost_equal(psf.aperture_circ(np.array([0.5, 0.5])), 0.980823770312415, 10)
-        np.testing.assert_almost_equal(psf.aperture_circ(np.array([0.9/np.sqrt(2), 0.9/np.sqrt(2)])), 0.7602499389065231, 10)
-        np.testing.assert_almost_equal(psf.aperture_circ(np.array([1.0/np.sqrt(2), 1.0/np.sqrt(2)])), 0.5, 10)
-        np.testing.assert_almost_equal(psf.aperture_circ(np.array([1.1/np.sqrt(2), 1.1/np.sqrt(2)])), 0.23975006109347657, 10)
+        
+        coords = np.array([[0.0, 0.0],
+                          [0.0, 0.5],
+                          [0.0, 0.9],
+                          [0.0, 1.0],
+                          [0.0, 1.1],
+                          [0.5, 0.5],
+                          [0.9/np.sqrt(2), 0.9/np.sqrt(2)],
+                          [1.0/np.sqrt(2), 1.0/np.sqrt(2)],
+                          [1.1/np.sqrt(2), 1.1/np.sqrt(2)]])
+        
+        expected = np.array([1.0,
+                             0.9997965239912775,
+                             0.7602499389065231,
+                             0.5,
+                             0.23975006109347657,
+                             0.980823770312415,
+                             0.7602499389065231,
+                             0.5,
+                             0.23975006109347657])
+    
+        for i in np.arange(0, len(expected)):
+            np.testing.assert_almost_equal(psf.aperture_circ(coords[i]), expected[i], 10)
 
         for x in np.linspace(-1, 1, 100):
             for y in np.linspace(-1, 1, 100):
                 np.testing.assert_almost_equal(psf.aperture_circ(np.array([x, y]), r=1.0, coef=1e5), psf.aperture_circ(np.array([x, y]), r=1.0, coef=0.0), 10)
+                
+                
+        # Test the whole array at once
+        np.testing.assert_almost_equal(psf.aperture_circ(coords), expected, 10)
+        
+        xs = np.linspace(-1.0, 1.0, 100)
+        ys = np.linspace(-1.0, 1.0, 100)
+        us = np.transpose([np.tile(xs, 100), np.repeat(ys, 100)])
+        np.testing.assert_almost_equal(psf.aperture_circ(us, r=1.0, coef=1e5), psf.aperture_circ(us, r=1.0, coef=0.0), 10)
+
         
 if __name__ == '__main__':
     unittest.main()
