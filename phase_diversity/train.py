06ea6bd0299549c38d98908495f0ab81cc283777
nigul <nigulolspert@gmail.com>
2019-01-23 09:52:23 +0100

    changes

--- phase_diversity/train.py
+++ phase_diversity/train.py
@@ -12,23 +12,25 @@ from matplotlib import cm
 nx = 320
 ny = 320
 
-zoomin_start = nx/2 - 33
-zoomin_end = ny/2 + 33
+zoomin_start = nx/2 - 37
+zoomin_end = ny/2 + 37
 
-psf_vals_nx = 66
-psf_vals_ny = 66
+psf_vals_nx = 74
+psf_vals_ny = 74
 
 
 aperture_func = lambda u: psf.aperture_circ(u, 0.2, 100.0)
 
 n_coefs = 10
-n_data = 3
+n_data = 20
 n_train = int(n_data*0.75)
 
-n_epochs = 500
+n_epochs = 1000
 
 def reverse_colourmap(cmap, name = 'my_cmap_r'):
      return mpl.colors.LinearSegmentedColormap(name, cm.revcmap(cmap._segmentdata))
+
+my_cmap = reverse_colourmap(plt.get_cmap('binary'))#plt.get_cmap('winter')
 
 def create_model():
     
@@ -40,19 +42,19 @@ def create_model():
     
     hidden1 = keras.layers.Dense(start_nx*start_ny, activation='relu')(coefs)
     hidden2 = keras.layers.Reshape((start_nx, start_ny, 1))(hidden1)
-    hidden3 = keras.layers.Conv2D(18, (4, 4), activation='relu', padding='same')(hidden2)
+    hidden3 = keras.layers.Conv2D(18, (3, 3), activation='relu', padding='same')(hidden2)
     hidden4 = keras.layers.UpSampling2D((2, 2))(hidden3)
-    hidden5 = keras.layers.Conv2D(8, (6, 6), activation='relu', padding='same')(hidden4)
+    hidden5 = keras.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(hidden4)
     hidden6 = keras.layers.UpSampling2D((2, 2))(hidden5)
-    hidden7 = keras.layers.Conv2D(4, (8, 8), activation='relu')(hidden6)
+    hidden7 = keras.layers.Conv2D(4, (4, 4), activation='relu')(hidden6)
     hidden8 = keras.layers.UpSampling2D((2, 2))(hidden7)
-    output = keras.layers.Conv2D(1, (10, 10), activation='sigmoid', padding='same')(hidden8)
+    output = keras.layers.Conv2D(1, (4, 4), activation='sigmoid', padding='same')(hidden8)
 
     model = keras.models.Model(input=coefs, output=output)
     optimizer = keras.optimizers.RMSprop(lr=0.00025, rho=0.95, epsilon=0.01)
     #model.compile(optimizer, loss='mse')
-    #model.compile(optimizer='adadelta', loss='binary_crossentropy')
-    model.compile(optimizer='adadelta', loss='mse')
+    model.compile(optimizer='adadelta', loss='binary_crossentropy')
+    #model.compile(optimizer='adadelta', loss='mse')
     return model
 
 
@@ -65,6 +67,49 @@ def train(coefs_train, coefs_test, psf_train, psf_test):
                 shuffle=True,
                 validation_data=(coefs_test, psf_test),
                 callbacks=[keras.callbacks.TensorBoard(log_dir='model_log')])
+    
+    ############################
+    # Plot training data results
+    n_test = 5
+    print np.shape(psf_train)
+    predicted_psfs = model.predict(coefs_train[0:n_test])
+    predicted_psfs = np.reshape(predicted_psfs, (n_test, psf_vals_nx, psf_vals_ny))
+    
+    extent=[0., 1., 0., 1.]
+    plot_aspect=(extent[1]-extent[0])/(extent[3]-extent[2])#*2/3 
+
+    fig, axes = plt.subplots(nrows=n_test, ncols=2)
+    fig.set_size_inches(6, 3*n_test)
+    
+    for i in np.arange(0, n_test):
+    
+        ax = axes[i][0]
+        #ax.imshow(np.log(psf_vals[i].T),extent=extent,cmap=my_cmap,origin='lower')
+        ax.imshow(np.reshape(psf_train[i], (psf_vals_nx, psf_vals_ny)).T,extent=extent,cmap=my_cmap,origin='lower')
+        #ax1.set_title(r'Factor graph')
+        #ax1.set_ylabel(r'$f$')
+        #start, end = ax32.get_xlim()
+        #ax1.xaxis.set_ticks(np.arange(5, end, 4.9999999))
+        #ax1.xaxis.set_major_formatter(ticker.FormatStrFormatter('%0.0f'))
+        #ax1.xaxis.labelpad = -1
+        #ax1.set_xlabel(r'$l_{\rm coh}$')#,fontsize=20)
+        ax.set_aspect(aspect=plot_aspect)
+
+        ax = axes[i][1]
+        #ax.imshow(np.log(predicted_psfs[i].T),extent=extent,cmap=my_cmap,origin='lower')
+        ax.imshow(predicted_psfs[i].T,extent=extent,cmap=my_cmap,origin='lower')
+        #ax1.set_title(r'Factor graph')
+        #ax1.set_ylabel(r'$f$')
+        #start, end = ax32.get_xlim()
+        #ax1.xaxis.set_ticks(np.arange(5, end, 4.9999999))
+        #ax1.xaxis.set_major_formatter(ticker.FormatStrFormatter('%0.0f'))
+        #ax1.xaxis.labelpad = -1
+        #ax1.set_xlabel(r'$l_{\rm coh}$')#,fontsize=20)
+        ax.set_aspect(aspect=plot_aspect)
+
+    fig.savefig('psf_train.png')
+    plt.close(fig)
+    
     return model
 
 def test(model):
@@ -85,10 +130,6 @@ def test(model):
         
         extent=[0., 1., 0., 1.]
         plot_aspect=(extent[1]-extent[0])/(extent[3]-extent[2])#*2/3 
-        
-        #my_cmap = reverse_colourmap(plt.get_cmap('gnuplot'))
-        my_cmap = reverse_colourmap(plt.get_cmap('binary'))#plt.get_cmap('winter')
-        
         
         fig, axes = plt.subplots(nrows=n_test_data, ncols=2)
         fig.set_size_inches(6, 3*n_test_data)
@@ -119,12 +160,12 @@ def test(model):
             #ax1.set_xlabel(r'$l_{\rm coh}$')#,fontsize=20)
             ax.set_aspect(aspect=plot_aspect)
 
-        fig.savefig('psf_nn.png')
+        fig.savefig('psf_test.png')
         plt.close(fig)
 
 def gen_data(n_data, normalize=True, log=False):
     #coefs = np.zeros((n_data, n_coefs))
-    coefs = np.random.normal(size=(n_data, n_coefs))
+    coefs = np.random.normal(size=(n_data, n_coefs))*10
     psf_vals = np.zeros((n_data, psf_vals_nx, psf_vals_ny))
     for i in np.arange(0, n_data):
         pa = psf.phase_aberration(zip(np.arange(4, n_coefs + 4), coefs[i]))
