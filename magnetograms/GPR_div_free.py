Fixed GPR_div_free gradient calculations

magnetograms/GPR_div_free.py
@@ -39,12 +39,11 @@ class GPR_div_free:
                         K_grads[1, i_abs, j_abs] = -2.0 * x_diff[i1] * x_diff[j1] * self.inv_length_scale_qb
                         if (i1 == j1):
                             K[i_abs, j_abs] += 1 - x_diff_sq * self.inv_length_scale_sq
-                            K_grads[1, i_abs, j_abs] += 1 + 2.0 * x_diff_sq * self.inv_length_scale_qb
+                            K_grads[1, i_abs, j_abs] += 2.0 * x_diff_sq * self.inv_length_scale_qb
                         exp_fact = np.exp(-0.5 * self.inv_length_scale_sq * x_diff_sq)
-                        K_grads[0, i_abs, j_abs] *= K[i_abs, j_abs] * exp_fact
-                        K_grads[1, i_abs, j_abs] *= self.sig_var
+                        K_grads[0, i_abs, j_abs] = K[i_abs, j_abs] * exp_fact
                         K_grads[1, i_abs, j_abs] += K[i_abs, j_abs] * x_diff_sq * self.inv_length_scale_qb
-                        K_grads[1, i_abs, j_abs] *= exp_fact
+                        K_grads[1, i_abs, j_abs] *= self.sig_var * exp_fact
                         K[i_abs, j_abs] *= self.sig_var * exp_fact
                 
         if data_or_test:

magnetograms/GPR_div_free_test.py
@@ -23,6 +23,9 @@ class test_GPR_div_feee(unittest.TestCase):
         
         K, K_grads = gp.calc_cov(x, x, data_or_test=True, calc_grad = True)
         
+        #######################################################################        
+        # Compare covariance
+        
         K_expected = np.zeros((8, 8))
         for i in np.arange(0, 4):
             for j in np.arange(0, 4):
@@ -35,12 +38,14 @@ class test_GPR_div_feee(unittest.TestCase):
                         K_expected[i_abs, j_abs] = x_diff[i1] * x_diff[j1] / ell / ell
                         if (i1 == j1):
                             K_expected[i_abs, j_abs] += 1 - x_diff_sq / ell / ell
-                        exp_fact = np.exp(-0.5 * x_diff_sq / ell / ell)
-                        K_expected[i_abs, j_abs] *= sig_var * exp_fact
+                        K_expected[i_abs, j_abs] *= sig_var * np.exp(-0.5 * x_diff_sq / ell / ell)
         K_expected += np.identity(8)*noise_var
         
         np.testing.assert_almost_equal(K, K_expected, 10)
                 
+        #######################################################################        
+        # Compare gradients of covariance
+
         K_grads_expected = np.zeros((3, 8, 8))
         for i in np.arange(0, 4):
             for j in np.arange(0, 4):
@@ -54,16 +59,51 @@ class test_GPR_div_feee(unittest.TestCase):
                         K_grads_expected[1, i_abs, j_abs] = -2.0 * x_diff[i1] * x_diff[j1] / ell / ell / ell
                         if (i1 == j1):
                             K_i_j += 1 - x_diff_sq / ell / ell
-                            K_grads_expected[1, i_abs, j_abs] += 1 + 2.0 * x_diff_sq / ell / ell / ell
+                            K_grads_expected[1, i_abs, j_abs] += 2.0 * x_diff_sq / ell / ell / ell
                         exp_fact = np.exp(-0.5 * x_diff_sq / ell / ell)
-                        K_grads_expected[0, i_abs, j_abs] *= K_i_j * exp_fact
-                        K_grads_expected[1, i_abs, j_abs] *= sig_var
+                        K_grads_expected[0, i_abs, j_abs] = K_i_j * exp_fact
                         K_grads_expected[1, i_abs, j_abs] += K_i_j * x_diff_sq / ell / ell / ell
-                        K_grads_expected[1, i_abs, j_abs] *= exp_fact
+                        K_grads_expected[1, i_abs, j_abs] *= sig_var * exp_fact
         K_grads_expected[2, :, :] += np.identity(8)
                 
         np.testing.assert_almost_equal(K_grads, K_grads_expected, 10)
+
+        #######################################################################        
+        # Compare gradients with values calculate using finite difference
+        delta_sig_var = sig_var*1.0e-5
+        delta_ell = ell * 1.0e-10
+        delta_noise_var = noise_var * 1.0e-5
+
+        gp1 = GPR_div_free.GPR_div_free(sig_var + delta_sig_var, ell, noise_var)
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = (K1 - K) / delta_sig_var
+        np.testing.assert_almost_equal(K_grads[0,:,:], K_grads_expected, 10)
         
+        gp1 = GPR_div_free.GPR_div_free(sig_var - delta_sig_var, ell, noise_var)
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = -(K1 - K) /delta_sig_var
+        np.testing.assert_almost_equal(K_grads[0,:,:], K_grads_expected, 10)
+
+        gp1 = GPR_div_free.GPR_div_free(sig_var, ell + delta_ell, noise_var)
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = (K1 - K) / delta_ell
+        np.testing.assert_almost_equal(K_grads[1,:,:], K_grads_expected, 9)
+        
+        gp1 = GPR_div_free.GPR_div_free(sig_var, ell - delta_ell, noise_var)
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = -(K1 - K) / delta_ell
+        np.testing.assert_almost_equal(K_grads[1,:,:], K_grads_expected, 9)
+
+        gp1 = GPR_div_free.GPR_div_free(sig_var, ell, noise_var + delta_noise_var)
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = (K1 - K) / delta_noise_var
+        np.testing.assert_almost_equal(K_grads[2,:,:], K_grads_expected, 10)
+        
+        gp1 = GPR_div_free.GPR_div_free(sig_var, ell, noise_var - delta_noise_var)
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = -(K1 - K) / delta_noise_var
+        np.testing.assert_almost_equal(K_grads[2,:,:], K_grads_expected, 10)
+
 
         
 if __name__ == '__main__':