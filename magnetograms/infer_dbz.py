9679ab59cf45eebf3f38f30979e1278f248eb6d6
nigul <nigulolspert@gmail.com>
2019-05-16 11:12:14 +0200

    changes

--- magnetograms/infer_dbz.py
+++ magnetograms/infer_dbz.py
@@ -21,28 +21,34 @@ class infer_dbz():
         self.div_xy_flat = np.reshape(div_xy, (n))
         self.bz_flat = np.reshape(bz, (n))
 
-        self.bz1 = np.zeros_like(self.bz_flat)
-        
-
         self.sig_var = 1.
         self.length_scale = 2.
         self.noise_var = 0.01
         
-        gp = cov_sq_exp.cov_sq_exp(self.sig_var, self.length_scale, self.noise_var)
+        self.gp = cov_sq_exp.cov_sq_exp(self.sig_var, self.length_scale, self.noise_var)
 
-        self.kgp = kiss_gp.kiss_gp(x, u_mesh, u, lambda _, _, _, u1, u2, data_or_test: gp.calc_cov(u, u2, data_or_test=data_or_test, calc_grad = True))
+        self.kgp = kiss_gp.kiss_gp(x, u_mesh, u, self.calc_cov)
+
+    def calc_cov(self, sig_var, length_scale, noise_var, u1, u2, data_or_test):
+        # Arguments are actually unused
+        assert(sig_var == self.sig_var)
+        assert(length_scale == self.length_scale)
+        assert(noise_var == self.noise_var)
+        K, K_grads = self.gp.calc_cov(u1, u2, data_or_test=data_or_test, calc_grad = False)
+        K1 = self.div_xy_flat*(self.div_xy_flat*K.T) + K
+        np.testing.assert_almost_equal(np.dot(self.div_xy_flat, np.dot(K, np.diag(self.div_xy_flat))), K1)
+        
+        K1_grads = np.zeros_like(K_grads)
+        for i in K1_grads.shape[0]:
+            K1_grads[i] = self.div_xy_flat*(self.div_xy_flat*K_grads[i].T) + K_grads[i]
+        
+        return K1, K1_grads
+
 
     def calc(self):
-        y1 = (self.bz_flat - self.bz1)/ (self.div_xy_flat + 1e-15)
 
-        _ = self.kgp.likelihood([self.length_scale, self.sig_var], [self.noise_var, y1])
-        dz_mean, f_var = self.kgp.fit(self.x, calc_var = False)
+        _ = self.kgp.likelihood([self.length_scale, self.sig_var], [self.noise_var, self.bz_flat])
+        bz_mean, f_var = self.kgp.fit(self.x, calc_var = False)
         
-        
-        y1 = self.bz_flat - self.div_xy_flat * dz_mean
-        _ = self.kgp.likelihood([self.length_scale, self.sig_var], [self.noise_var, y1])
-        bz1,  _ = self.kgp.fit(self.x, calc_var = False)
-        self.bz1 = bz1
-        
-        
+        return self.bz_flat - bz_mean
         
