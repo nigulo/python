b13a6418baacb0d221d64169a6c43f471b3f1fd4
nigulo <nigulolspert@gmail.com>
2020-01-04 10:27:18 +0100

    changes

--- phase_diversity/learn_object.py
+++ phase_diversity/learn_object.py
@@ -126,7 +126,7 @@ class nn_model:
         #if not full:
         history = model.fit(self.Ds_train, self.objs_train,
                     epochs=n_epochs,
-                    batch_size=None,
+                    batch_size=1,
                     shuffle=True,
                     validation_data=(self.Ds_validation, self.objs_validation),
                     #callbacks=[keras.callbacks.TensorBoard(log_dir='model_log')],
