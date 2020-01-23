e746cfad0ffc6b52f2d8e085c27e96154f1f1351
nigul <nigulolspert@gmail.com>
2020-01-23 11:42:52 +0100

    Adding scale difference support to cov_sq_exp

--- magnetograms/cov_sq_exp.py
+++ magnetograms/cov_sq_exp.py
@@ -3,7 +3,7 @@ from cov_func import cov_func
 
 class cov_sq_exp(cov_func):
     
-    def __init__(self, sig_var, length_scale, noise_var, dim_out, toeplitz = False):
+    def __init__(self, sig_var, length_scale, noise_var, dim_out, toeplitz = False, scales = None):
         super(cov_sq_exp, self).__init__(toeplitz=toeplitz)
         self.sig_var = sig_var
         self.length_scale = length_scale
@@ -11,6 +11,7 @@ class cov_sq_exp(cov_func):
         self.inv_length_scale_qb = self.inv_length_scale_sq/length_scale
         self.noise_var = noise_var
         self.dim_out = dim_out
+        self.scales = scales
 
     def calc_cov(self, x1, x2, data_or_test, calc_grad = False):
         if calc_grad:
@@ -18,24 +19,40 @@ class cov_sq_exp(cov_func):
             assert(np.isscalar(self.noise_var))
         K = np.zeros((np.shape(x1)[0]*self.dim_out, np.shape(x2)[0]*self.dim_out))
         assert(x1.shape[1] == x2.shape[1])
+        scales = np.ones(x1.shape[1])
+        if self.scales is not None:
+            assert(len(self.scales) == x1.shape[1])
+            for k in np.arange(len(self.scales)):
+                if self.scales[k] is not None:
+                    scales[k] = self.scales[k]
         # Gradients for sigvar, length_scale and noise_var
         I = np.identity(self.dim_out)
         if calc_grad:
-            K_grads = np.zeros((3, np.shape(x1)[0]*self.dim_out, np.shape(x2)[0]*self.dim_out))
+            if self.scales is not None:
+                K_grads = np.zeros((3+len(self.scales), np.shape(x1)[0]*self.dim_out, np.shape(x2)[0]*self.dim_out))
+            else:
+                K_grads = np.zeros((3, np.shape(x1)[0]*self.dim_out, np.shape(x2)[0]*self.dim_out))
         for i in np.arange(0, np.shape(x1)[0]):
             i_abs_1 = self.dim_out*i
             i_abs_2 = i_abs_1 + self.dim_out
             for j in np.arange(0, np.shape(x2)[0]):
-                x_diff = x1[i] - x2[j]
-                x_diff_sq = np.dot(x_diff, x_diff)
-                exp_fact = np.exp(-0.5 * self.inv_length_scale_sq * x_diff_sq)
+                x_diff = (x1[i] - x2[j])*scales
+                x_diff_sq = x_diff*x_diff
+                x_diff_sq_sum = np.sum(x_diff_sq)
+                exp_fact = np.exp(-0.5 * self.inv_length_scale_sq * x_diff_sq_sum)
                 j_abs_1 = self.dim_out*j
                 j_abs_2 = j_abs_1 + self.dim_out
                 K[i_abs_1:i_abs_2, j_abs_1:j_abs_2] = I * exp_fact
                 if calc_grad:
                     K_grads[0, i_abs_1:i_abs_2, j_abs_1:j_abs_2] = K[i_abs_1:i_abs_2, j_abs_1:j_abs_2]
-                    K_grads[1, i_abs_1:i_abs_2, j_abs_1:j_abs_2] = K[i_abs_1:i_abs_2, j_abs_1:j_abs_2] * x_diff_sq * self.inv_length_scale_qb
+                    K_grads[1, i_abs_1:i_abs_2, j_abs_1:j_abs_2] = K[i_abs_1:i_abs_2, j_abs_1:j_abs_2] * x_diff_sq_sum * self.inv_length_scale_qb
                     K_grads[1, i_abs_1:i_abs_2, j_abs_1:j_abs_2] *= self.sig_var
+                    if self.scales is not None:
+                        for k in np.arange(len(self.scales)):
+                            if self.scales[k] is not None:
+                                K_grads[3:3+k, i_abs_1:i_abs_2, j_abs_1:j_abs_2] = -K[i_abs_1:i_abs_2, j_abs_1:j_abs_2] * x_diff_sq[k] * scales[k]
+                                K_grads[3:3+k, i_abs_1:i_abs_2, j_abs_1:j_abs_2] *= self.sig_var
+                                
                 K[i_abs_1:i_abs_2, j_abs_1:j_abs_2] *= self.sig_var
                 
         if data_or_test:
--- magnetograms/tests/cov_sq_exp_test.py
+++ magnetograms/tests/cov_sq_exp_test.py
@@ -52,8 +52,10 @@ class test_cov_sq_exp(unittest.TestCase):
                 exp_fact = np.exp(-0.5 * x_diff_sq / ell / ell)
                 K_grads_expected[1, i_abs:i_abs+2, j_abs:j_abs+2] = np.identity(2) * sig_var * exp_fact * x_diff_sq / ell / ell / ell
                 K_grads_expected[0, i_abs:i_abs+2, j_abs:j_abs+2] = np.identity(2) * exp_fact
-        K_grads_expected[2, :, :] += np.identity(8)
+        K_grads_expected[2, :, :] = np.identity(8)
 
+        print(K_grads)
+        print(K_grads_expected)
         np.testing.assert_almost_equal(K_grads, K_grads_expected, 7)
 
         #######################################################################        
@@ -92,6 +94,107 @@ class test_cov_sq_exp(unittest.TestCase):
         K_grads_expected = -(K1 - K) / delta_noise_var
         np.testing.assert_almost_equal(K_grads[2,:,:], K_grads_expected, 10)
 
+    def test_calc_cov_with_scales(self):
+        
+        x1_range = 1.0
+        x2_range = 1.0
+        n1 = 2
+        n2 = 2
+        x1 = np.linspace(0, x1_range, n1)
+        x2 = np.linspace(0, x2_range, n2)
+        x_mesh = np.meshgrid(x1, x2)
+        x = np.dstack(x_mesh).reshape(-1, 2)
+
+        sig_var = 0.5
+        ell = 0.2
+        noise_var = 0.07
+        
+        scale_y = 7.81
+        gp = cov_sq_exp.cov_sq_exp(sig_var, ell, noise_var, dim_out=2, scales=[None, scale_y])
+        
+        K, K_grads = gp.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        
+        #######################################################################        
+        # Compare covariance
+        
+        K_expected = np.zeros((8, 8))
+        for i in np.arange(0, 4):
+            i_abs = 2*i
+            for j in np.arange(0, 4):
+                x_diff = x[i] - x[j]
+                x_diff[1] *= scale_y
+                x_diff_sq = np.dot(x_diff, x_diff)
+                j_abs = 2*j
+                K_expected[i_abs:i_abs+2, j_abs:j_abs+2] = np.identity(2) * sig_var * np.exp(-0.5 * x_diff_sq / ell / ell)
+        K_expected += np.identity(8)*noise_var
+        
+        np.testing.assert_almost_equal(K, K_expected, 7)
+                
+        #######################################################################        
+        # Compare gradients of covariance
+
+        K_grads_expected = np.zeros((5, 8, 8))
+        for i in np.arange(0, 4):
+            i_abs = 2*i
+            for j in np.arange(0, 4):
+                x_diff = x[i] - x[j]
+                x_diff[1] *= scale_y
+                x_diff_sq = np.dot(x_diff, x_diff)
+                j_abs = 2*j
+                exp_fact = np.exp(-0.5 * x_diff_sq / ell / ell)
+                K_grads_expected[0, i_abs:i_abs+2, j_abs:j_abs+2] = np.identity(2) * exp_fact
+                K_grads_expected[1, i_abs:i_abs+2, j_abs:j_abs+2] = np.identity(2) * sig_var * exp_fact * x_diff_sq / ell / ell / ell
+                K_grads_expected[4, i_abs:i_abs+2, j_abs:j_abs+2] = -np.identity(2) * sig_var * exp_fact * x_diff[1] * x_diff[1] * scale_y
+        K_grads_expected[2, :, :] = np.identity(8)
+
+        np.testing.assert_almost_equal(K_grads, K_grads_expected, 7)
+
+        #######################################################################        
+        # Compare gradients with values calculate using finite difference
+        delta_sig_var = sig_var*1.0e-5
+        delta_ell = ell * 1.0e-10
+        delta_noise_var = noise_var * 1.0e-5
+        delta_scale_y = scale_y * 1.0e-5
+
+        gp1 = cov_sq_exp.cov_sq_exp(sig_var + delta_sig_var, ell, noise_var, dim_out=2, scales=[None, scale_y])
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = (K1 - K) / delta_sig_var
+        np.testing.assert_almost_equal(K_grads[0,:,:], K_grads_expected, 10)
+        
+        gp1 = cov_sq_exp.cov_sq_exp(sig_var - delta_sig_var, ell, noise_var, dim_out=2, scales=[None, scale_y])
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = -(K1 - K) /delta_sig_var
+        np.testing.assert_almost_equal(K_grads[0,:,:], K_grads_expected, 10)
+
+        gp1 = cov_sq_exp.cov_sq_exp(sig_var, ell + delta_ell, noise_var, dim_out=2, scales=[None, scale_y])
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = (K1 - K) / delta_ell
+        np.testing.assert_almost_equal(K_grads[1,:,:], K_grads_expected, 9)
+        
+        gp1 = cov_sq_exp.cov_sq_exp(sig_var, ell - delta_ell, noise_var, dim_out=2, scales=[None, scale_y])
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = -(K1 - K) / delta_ell
+        np.testing.assert_almost_equal(K_grads[1,:,:], K_grads_expected, 9)
+
+        gp1 = cov_sq_exp.cov_sq_exp(sig_var, ell, noise_var + delta_noise_var, dim_out=2, scales=[None, scale_y])
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = (K1 - K) / delta_noise_var
+        np.testing.assert_almost_equal(K_grads[2,:,:], K_grads_expected, 10)
+        
+        gp1 = cov_sq_exp.cov_sq_exp(sig_var, ell, noise_var - delta_noise_var, dim_out=2, scales=[None, scale_y])
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = -(K1 - K) / delta_noise_var
+        np.testing.assert_almost_equal(K_grads[2,:,:], K_grads_expected, 10)
+
+        gp1 = cov_sq_exp.cov_sq_exp(sig_var, ell, noise_var, dim_out=2, scales=[None, scale_y + delta_scale_y])
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = (K1 - K) / delta_noise_var
+        np.testing.assert_almost_equal(K_grads[4,:,:], K_grads_expected, 9)
+        
+        gp1 = cov_sq_exp.cov_sq_exp(sig_var, ell, noise_var, dim_out=2, scales=[None, scale_y - delta_scale_y])
+        K1, _ = gp1.calc_cov(x, x, data_or_test=True, calc_grad = True)
+        K_grads_expected = -(K1 - K) / delta_noise_var
+        np.testing.assert_almost_equal(K_grads[4,:,:], K_grads_expected, 9)
 
         
 if __name__ == '__main__':
