316152c3f0313fcdf7003f7e8120b9dfbd5f6f7a
nigulo <nigulolspert@gmail.com>
2020-03-03 21:33:38 +0100

    changes

--- phase_diversity/learn_mfbd.py
+++ phase_diversity/learn_mfbd.py
@@ -28,7 +28,7 @@ gamma = 1.0
 # How many frames to use in training
 num_frames = 20
 # How many objects to use in training
-num_objs = 100#None
+num_objs = 75#None
 
 # How many frames of the same object are sent to NN input
 # Must be power of 2
