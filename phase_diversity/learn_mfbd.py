beacac69b6098a503a9dc8bb6e381677d5b2df30
nigulo <nigulolspert@gmail.com>
2020-03-19 12:50:03 +0100

    bugfix

--- phase_diversity/learn_mfbd.py
+++ phase_diversity/learn_mfbd.py
@@ -307,7 +307,7 @@ class nn_model:
         ctf.set_phase_aberr(pa)
         ctf.set_pupil(pupil)
         #ctf.set_diversity(diversity[i, j])
-        batch_size_per_gpu = batch_size//max(1, n_gpus)
+        batch_size_per_gpu = max(1, batch_size//max(1, n_gpus))
         self.psf = psf_tf.psf_tf(ctf, num_frames=num_frames_input, batch_size=batch_size_per_gpu, set_diversity=True)
         print("batch_size_per_gpu, num_frames_input", batch_size_per_gpu, num_frames_input)
         
