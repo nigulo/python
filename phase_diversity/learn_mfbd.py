baaaaeb1c56d52186395ecec3d7f74018b3804b1
nigulo <nigulolspert@gmail.com>
2020-04-21 19:11:45 +0200

    bugfix

--- phase_diversity/learn_mfbd.py
+++ phase_diversity/learn_mfbd.py
@@ -659,6 +659,8 @@ class nn_model:
     def Ds_reconstr(self, Ds, alphas, diversity):
         image_deconv = self.deconvolve(Ds, alphas, diversity, do_fft=False)
         with tf.device(gpu_id):
+            image_deconv = tf.reshape(image_deconv, [alphas.shape[0], 1, self.nx, self.nx])
+            image_deconv = tf.tile(image_deconv, [1, alphas.shape[1], 1, 1])
             alphas = tf.constant(alphas, dtype='float32')
             return self.psf_test.Ds_reconstr2(image_deconv, alphas)
         
