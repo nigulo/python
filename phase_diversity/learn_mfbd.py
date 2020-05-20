6426df789aa9a1475ccf51453e6e8abce63c2c03
nigulo <nigulolspert@gmail.com>
2020-05-20 10:56:35 +0200

    changes

--- phase_diversity/learn_mfbd.py
+++ phase_diversity/learn_mfbd.py
@@ -559,7 +559,7 @@ class nn_model:
                 #alphas_layer = keras.layers.Dense(128, activation='relu')(alphas_layer)
                 #alphas_layer = keras.layers.Dense(jmax*num_frames_input, activation='linear')(alphas_layer)
                 alphas_layer = tf.reshape(alphas_layer, [1, batch_size_per_gpu, 1024])
-                lstm = tf.keras.layers.LSTM(512, return_sequences=True)#, return_state=True)
+                lstm = tf.keras.layers.LSTM(512, return_sequences=True, stateful=True)#, return_state=True)
                 alphas_layer = lstm(alphas_layer)
                 alphas_layer = tf.reshape(alphas_layer, [batch_size_per_gpu, 512])
                 #alphas_layer = seq_block(alphas_layer)
