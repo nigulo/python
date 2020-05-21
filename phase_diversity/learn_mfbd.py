12178638e2cb6800925d3e0cef0b9a011573ff5c
nigulo <nigulolspert@gmail.com>
2020-05-21 13:55:20 +0200

    changes

--- phase_diversity/learn_mfbd.py
+++ phase_diversity/learn_mfbd.py
@@ -561,13 +561,14 @@ class nn_model:
                 #alphas_layer = keras.layers.Dense(jmax*num_frames_input, activation='linear')(alphas_layer)
                 if no_shuffle:
                     alphas_layer = tf.reshape(alphas_layer, [1, batch_size_per_gpu, 1024])
-                    lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(512, return_sequences=True, stateful=True), merge_mode='sum')#, activation="relu")#, return_state=True)
+                    lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(512, return_sequences=True, stateful=True))#, merge_mode='sum')#, activation="relu")#, return_state=True)
                     #lstm2 = tf.keras.layers.LSTM(512, return_sequences=True, stateful=True, go_backwards=True)#, activation="relu")#, return_state=True)
                     alphas_layer = lstm(alphas_layer)
                     #alphas_layer = lstm2(alphas_layer)
-                    alphas_layer = tf.reshape(alphas_layer, [batch_size_per_gpu, 512])
+                    alphas_layer = tf.reshape(alphas_layer, [batch_size_per_gpu, 1024])
                 #alphas_layer = seq_block(alphas_layer)
                 
+                alphas_layer = keras.layers.Dense(512, activation='relu')(alphas_layer)
                 alphas_layer = keras.layers.Dense(jmax*num_frames_input, activation='linear')(alphas_layer)
                 #if nn_mode >= MODE_2:
                 #    alphas_layer1 = keras.layers.Dense(1024, activation='relu')(alphas_input)
