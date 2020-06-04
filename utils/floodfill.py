716e3d98da40d6d9757825655c43ed53dbddfa99
nigulo <nigulolspert@gmail.com>
2020-06-04 15:45:15 +0200

    bugfix

--- utils/floodfill.py
+++ utils/floodfill.py
@@ -8,7 +8,7 @@ class floodfill:
         self.closedRegions = set()
         self.labels = np.zeros_like(mat)
         self.label = 1
-        self.neighbours = dict()
+        self.neighbous = dict()
         self.regionAreas = dict()
         self.regionExtents = dict()
         self.compFunc = compFunc
