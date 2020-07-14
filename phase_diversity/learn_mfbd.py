524e59ee2be74c6ed7c5873a6400b1ca21f6dab5
nigulo <nigulolspert@gmail.com>
2020-07-14 13:28:36 +0200

    bugfix

--- phase_diversity/learn_mfbd.py
+++ phase_diversity/learn_mfbd.py
@@ -583,7 +583,7 @@ class nn_model:
                     lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(size//2, return_sequences=True, stateful=True))#, merge_mode='sum')#, activation="relu")#, return_state=True)
                     #lstm2 = tf.keras.layers.LSTM(512, return_sequences=True, stateful=True, go_backwards=True)#, activation="relu")#, return_state=True)
                     alphas_layer1 = lstm(alphas_layer1)
-                    alphas_layer1 = tf.reshape(alphas_layer1, [batch_size_per_gpu, size])
+                    alphas_layer = tf.reshape(alphas_layer1, [batch_size_per_gpu, size])
                     #alphas_layer = tf.reshape(alphas_layer, [batch_size_per_gpu, 512])
                     #alphas_layer = tf.reshape(alphas_layer, [batch_size_per_gpu*2, 512])
                     #alphas_layer1 = tf.slice(alphas_layer, [0, 0, 0], [1, batch_size_per_gpu, 512])
