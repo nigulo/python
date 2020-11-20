93d857f56b8f2decd171373612b6a63927b1af4d
nigul <nigulolspert@gmail.com>
2020-11-20 13:36:15 +0100

    changes

--- phase_diversity/psf_torch.py
+++ phase_diversity/psf_torch.py
@@ -730,7 +730,7 @@ class psf_torch():
             DD1 = DD + DD1 
             if self.sum_over_batch:
                 DD1 = torch.sum(DD1, axis=0)
-            loss = (DD1 - (num + self.eps)/(den + self.eps))*H
+            loss = (DD1 - num/(den + self.eps))*H
             
             #if self.sum_over_batch:
             #    loss = tf.tile(tf.reshape(loss, [1, 1, nx, nx]), [self.batch_size, 1, 1, 1])
