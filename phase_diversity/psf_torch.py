8e2fa5d29d73b36dd83249dde52f51e6eb975a7c
nigul <nigulolspert@gmail.com>
2020-11-23 11:21:56 +0100

    bugfix

--- phase_diversity/psf_torch.py
+++ phase_diversity/psf_torch.py
@@ -579,7 +579,7 @@ class psf_torch():
             loss += torch.sum(DD)
         
         H = self.calc_filter(mul(DP, DP_conj), PP)
-        H = torch.from_numpy(utils.smart_fltr(H.cpu().numpy())).to(self.device, dtype=torch.float32)
+        H = to_complex(torch.from_numpy(utils.smart_fltr(real(H).cpu().numpy())).to(self.device, dtype=torch.float32))
         F_image = mul(F_image, H)
         
         #if self.fltr is not None:
