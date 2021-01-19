51a1a03aabb368138be9eb68c6d5a7c7826acd8f
nigulo <nigulolspert@gmail.com>
2021-01-19 16:14:27 +0200

    changes

--- fmode/quiet.py
+++ fmode/quiet.py
@@ -5,9 +5,6 @@ sys.path.append('..')
 import config
 import matplotlib as mpl
 
-mpl.use('Agg')
-mpl.rcParams['figure.figsize'] = (20, 30)
-#import pickle
 import numpy as np
 import numpy.random as random
 import time
@@ -20,6 +17,9 @@ import astropy.units as u
 from astropy.coordinates import SkyCoord
 import sunpy.coordinates
 from sunpy.coordinates import frames
+
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as axes3d
 
 A = 14.713
 B = -2.396
@@ -53,6 +53,37 @@ def center_and_radius(snapshot):
     print(xl, xr, yb, yt)
     print(xc, yc, r)
     return xc, yc, r
+
+def _plot_segment(longs, lats, ax, cmap, alpha):
+    phi, theta = np.meshgrid(longs*np.pi/180, (90-lats)*np.pi/180)
+    X = np.sin(theta) * np.cos(phi)
+    Y = np.sin(theta) * np.sin(phi)
+    Z = np.cos(theta)
+    plot = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cmap, linewidth=0, antialiased=False, alpha=alpha)
+
+def plot_segment(longs, lats):
+    longs_all = np.linspace(0, 360, 100)
+    lats_all = np.linspace(-90, 90, 50)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1, projection='3d')
+    _plot_segment(longs_all, lats_all, ax, None, alpha=0.2)#plt.get_cmap('Greys'))
+    _plot_segment(longs, lats, ax, plt.get_cmap('jet'), alpha=0.8)
+    fig.savefig("segment.png")
+    plt.close(fig)
+
+def do_plot(xs, ys):
+    print(xs.shape, ys.shape)
+    test_plot = plot.plot(nrows=1, ncols=1, size=plot.default_size(100, 100))
+    r = 900.
+    phi = np.linspace(0, np.pi*2, 100)
+    xs1 = r*np.cos(phi)
+    ys1 = r*np.sin(phi)
+    test_plot.plot(xs1, ys1, params="k-")
+    test_plot.plot(xs, ys, params="k.")
+    test_plot.save(f"segment_ortho.png")
+    test_plot.close()
+
 
 class quiet_sun:
     
@@ -108,8 +139,8 @@ class quiet_sun:
             #self.all_files.extend(files)
         self.all_files.sort()
         self.current_day = 0
+
         
-            
     def fetch_next(self):
         if self.num_days <= 0 or self.current_day <= self.num_days:
             day = self.all_files[self.current_day][:10]            
@@ -135,7 +166,7 @@ class quiet_sun:
             f = 1./snapshots_per_day
             self.nt = int(snapshots_per_day*self.num_hrs/24)
             #nx_full, ny_full = hdul[1].shape
-            coef1 = r/90.
+            #coef1 = r/90.
             #x = xc + (self.long - self.size/2)*coef
 
             #c = SkyCoord((self.long - self.size/2)*u.deg, (self.lat - self.size/2)*u.deg, frame=frames.Helioprojective, obstime="2013-02-14 00:00:00", observer="earth")
@@ -145,19 +176,39 @@ class quiet_sun:
             
             long1 = self.long - self.size/2
             long2 = long1 + self.size
+            num_long = int(r*self.size/90)
+            num_lat = num_long
+            longs = np.linspace(long1, long2, num_long)
+            lats = np.linspace(lat1, lat2, num_lat)
+            plot_segment(longs, lats)
             
+            long_lat = np.transpose([np.tile(longs, len(lats)), np.repeat(lats, len(longs))])
+
+            c0 = SkyCoord(long_lat[:, 0]*u.deg, long_lat[:, 1]*u.deg, frame=frames.HeliographicStonyhurst)
+            hpc_out = sunpy.coordinates.Helioprojective(observer="earth", obstime=f"{day} 00:00:00")
+            c1 = c0.transform_to(hpc_out)
+            
+            xs = c1.Tx.value
+            ys = c1.Ty.value
+            dists = c1.distance.value
+            r_arcsec = np.arctan(radius_km / dists[0])*180/np.pi*3600
+            coef = r/r_arcsec
+            print(r_arcsec, coef)
+            #xys = np.transpose([np.tile(xs, len(ys)), np.repeat(ys, len(xs))])
+            nx = len(longs)
+            ny = len(lats)
+            do_plot(xs, ys)
+
+            '''
             c0 = SkyCoord(long1*u.deg, lat1*u.deg, frame=frames.HeliographicStonyhurst)
             hpc_out = sunpy.coordinates.Helioprojective(observer="earth", obstime=f"{day} 00:00:00")
             c1 = c0.transform_to(hpc_out)
             
             x1 = c1.Tx.value
             y1 = c1.Ty.value
-            dist = c1.distance.value
-            r_arcsec = np.arctan(radius_km / dist)*180/np.pi*3600
-            coef = r/r_arcsec
-            print(r_arcsec, coef)
             
             c0 = SkyCoord(long2*u.deg, lat2*u.deg, frame=frames.HeliographicStonyhurst)
+            print(c0)
             hpc_out = sunpy.coordinates.Helioprojective(observer="earth", obstime=f"{day} 00:00:00")
             c1 = c0.transform_to(hpc_out)
             
@@ -167,6 +218,9 @@ class quiet_sun:
             nx = int((x2-x1)*coef)
             ny = int((y2-y1)*coef)
 
+            print(x1, x2, y1, y2)
+            '''
+            
             print(nx, ny)
             self.current_day += 1
             self.data = np.empty((len(hdul) - 1, ny, nx), dtype=np.float32)
@@ -182,20 +236,21 @@ class quiet_sun:
                 data = fits.getdata(file, i)
                 obstime = f"{day} {hrs}:{mins}:{secs}"
                 print(obstime)
-                ys = np.linspace(y1, y2, ny)
-                xs = np.linspace(x1, x2, nx)
                 
-                xys = np.transpose([np.tile(xs, len(ys)), np.repeat(ys, len(xs))])
+                #ys = np.linspace(y1, y2, ny)
+                #xs = np.linspace(x1, x2, nx)
+                
+                #xys = np.transpose([np.tile(xs, len(ys)), np.repeat(ys, len(xs))])
 
-                c1 = SkyCoord(xys[:, 0]*u.arcsec, xys[:, 1]*u.arcsec, frame=frames.Helioprojective, obstime=f"{day} 00:00:00", observer="earth")
+                c1 = SkyCoord(xs*u.arcsec, ys*u.arcsec, frame=frames.Helioprojective, obstime=f"{day} 00:00:00", observer="earth")
                 c2 = c1.transform_to(frames.HeliographicCarrington)
                 c3 = SkyCoord(c2.lon, c2.lat, frame=frames.HeliographicCarrington, obstime=obstime, observer="earth")
                 c4 = c3.transform_to(frames.Helioprojective)
-                print(c4)
+                #print(c4)
                 
                 x_pix = (c4.Tx.value*coef + xc).astype(int)
                 y_pix = (c4.Ty.value*coef + yc).astype(int)
-                print(x_pix)
+                #print(x_pix)
                 
                 l = 0
                 for j in np.arange(ny):
@@ -219,7 +274,7 @@ class quiet_sun:
                 
                         self.data[i-1, j, k] = data[y_pix[l], x_pix[l]]
                         l += 1
-                test_plot = plot.plot(nrows=1, ncols=1)
+                test_plot = plot.plot(nrows=1, ncols=1, size=plot.default_size(self.data[i-1].shape[1], self.data[i-1].shape[0]))
                 test_plot.colormap(self.data[i-1], cmap_name="bwr")
                 test_plot.save(f"test{i-1}.png")
                 test_plot.close()
@@ -230,80 +285,6 @@ class quiet_sun:
         else:
             raise "No more files"
         
-    '''
-    def fetch_next(self):
-        if self.num_days <= 0 or self.current_day <= self.num_days:    
-            file = self.path + "/" + self.all_files[self.current_day]
-            print(file)
-            hdul = fits.open(file)
-            hdul.info()
-            print(self.track)
-            full_snapshot = fits.getdata(file, 1)
-            #test_plot = plot.plot(nrows=1, ncols=1)
-            #indices = np.argwhere(np.isnan(full_snapshot))
-            #print(indices)
-            #full_snapshot[indices] = 0.
-            #print(np.max(full_snapshot), np.min(full_snapshot))
-            #test_plot.colormap(misc.trunc(full_snapshot, 1e-3), cmap_name="bwr")
-            #test_plot.save(f"full_snapshot.png")
-            #test_plot.close()
-            xc, yc, r = center_and_radius(full_snapshot)
-            
-            snapshots_per_day = len(hdul) - 1
-            f = 1./snapshots_per_day
-            self.nt = int(snapshots_per_day*self.num_hrs/24)
-            #nx_full, ny_full = hdul[1].shape
-            coef1 = r/90.
-            #x = xc + (self.long - self.size/2)*coef
-
-            y1 = int(r*np.sin(self.lat - self.size/2))#lat*coef1
-            y2 = int(r*np.sin(self.lat + self.size/2))#lat*coef1
-            ny = y2 - y1
-
-            r0 = r*np.cos(self.lat)
-            x1 = int(r0*np.sin(self.long - self.size/2))#lat*coef1
-            x2 = int(r0*np.sin(self.long + self.size/2))#lat*coef1
-            nx = x2 - x1
-
-            #nx = int(coef1*self.size)
-            #ny = int(y_coef*self.size)
-            print(nx)
-            self.current_day += 1
-            self.data = np.empty((len(hdul) - 1, ny, nx), dtype=np.float32)
-            for i in np.arange(1, len(hdul)):
-                lat = self.lat + self.size/2
-                y = r*np.sin(lat)#lat*coef1
-                data = fits.getdata(file, i)
-                if self.track:
-                    for j in np.arange(ny):
-                        lat = np.arcsin(y/r)#y/coef1
-                        r1 = r*np.cos(lat)
-                        coef2 = r1/90.
-                        long_shift = -diff_rot(lat)*(i-1)*f
-                        y1 = int(y + yc)                    
-                        long = self.long + self.size/2
-                        #x = long*coef2
-                        x = r0*np.sin(long)#lat*coef1
-                        for k in np.arange(nx):
-                            long = np.arcsin(x/r1) + long_shift#x/coef2 + long_shift
-                            print(lat*180/np.pi, long*180/np.pi)
-                            x1 = int(xc + r1*np.sin(long))
-                            self.data[i-1, j, k] = data[y1, x1]
-                            x -= 1
-                        y -= 1
-                else:
-                    self.data[i-1] = data[y1:y2, x1:x2]
-                test_plot = plot.plot(nrows=1, ncols=1)
-                test_plot.colormap(self.data[i-1], cmap_name="bwr")
-                test_plot.save(f"test{i-1}.png")
-                test_plot.close()
-                print(i)
-            print(self.data.shape)
-            hdul.close()
-            self.current_day += 1
-        else:
-            raise "No more files"
-    '''
     
     def is_quiet(self, t0):        
         return True
