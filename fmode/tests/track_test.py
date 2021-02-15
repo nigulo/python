fixed kurtosis calculation

fmode/tests/track_test.py
@@ -301,7 +301,9 @@ class test_collect_stats(unittest.TestCase):
         data = np.array([])
         
         for i in range(20):
-            data_i = np.random.normal(size=100)
+            data_i = np.random.normal(loc=np.random.normal()*10, scale=np.random.random()*10+1, size=100) + \
+                np.random.normal(loc=np.random.normal()*10, scale=np.random.random()*10+1, size=100) + \
+                np.random.normal(loc=np.random.normal()*10, scale=np.random.random()*10+1, size=100)
             data = np.append(data, data_i)
             stats[0, 0] += track.collect_stats_1(data_i)
             
@@ -315,7 +317,7 @@ class test_collect_stats(unittest.TestCase):
         np.testing.assert_almost_equal(mean, expected_mean)
         np.testing.assert_almost_equal(std, expected_std)
         np.testing.assert_almost_equal(skew, expected_skew)
-        np.testing.assert_almost_equal(kurt, expected_kurt, 5)
+        np.testing.assert_almost_equal(kurt, expected_kurt)
         
         data = np.abs(data)
 
@@ -323,11 +325,12 @@ class test_collect_stats(unittest.TestCase):
         expected_std = np.std(data)
         expected_skew = scipy.stats.skew(data)
         expected_kurt = scipy.stats.kurtosis(data)
-
+        
         np.testing.assert_almost_equal(abs_mean, expected_mean)
         np.testing.assert_almost_equal(abs_std, expected_std)
         np.testing.assert_almost_equal(abs_skew, expected_skew)
         np.testing.assert_almost_equal(abs_kurt, expected_kurt)
 
+        
 if __name__ == '__main__':
     unittest.main()

fmode/track.py
@@ -69,9 +69,9 @@ def collect_stats_2(data):
     abs_std = np.sqrt(abs_var)
     abs_std3 = abs_var * abs_std
     skew = (data3_mean - 3*mean*var - mean3)/std3
-    kurt = (data4_mean - mean*(2*data3_mean - mean3))/(var**2) - 2*mean*skew - 3
+    kurt = (data4_mean + mean*(6*mean*data2_mean - 4*data3_mean - 3*mean3))/(var**2) - 3
     abs_skew = (abs_data3_mean - 3*abs_mean*abs_var - abs_mean3)/abs_std3
-    abs_kurt = (data4_mean - abs_mean*(2*abs_data3_mean - abs_mean3))/(abs_var**2) - 2*abs_mean*abs_skew - 3
+    abs_kurt = (data4_mean + abs_mean*(6*abs_mean*data2_mean - 4*abs_data3_mean - 3*abs_mean3))/(abs_var**2) - 3
     
     return [mean, std, skew, kurt, abs_mean, abs_std, abs_skew, abs_kurt]
 