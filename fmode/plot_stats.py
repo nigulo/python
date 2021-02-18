9679abcb87241415f4003cd46abdf901ab470b78
olspern1 <nigul.olspert@aalto.fi>
2021-02-18 13:23:13 +0200

    changes

--- fmode/plot_stats.py
+++ fmode/plot_stats.py
@@ -11,6 +11,7 @@ import numpy as np
 import os.path
 from astropy.io import fits
 import plot
+import misc
 
 
 path = "output"
@@ -30,13 +31,21 @@ for file in all_files[::24]:
     try:
         hdul = fits.open(path + "/" + file)
         date = file[:10]
-        hdul2 = fits.open(path2 + "/" + date + "_hmi.M_720s")
-        time = date + " " + file[11:]
+        hdul2 = fits.open(path2 + "/" + date + "_hmi.M_720s.fits")
+        time = file[11:19]
         for i in range(1, len(hdul2)):
-            if hdul2[i].header['T_REC'] == time:
+            if hdul2[i].header['T_REC'][11:19] == time:
                 input_data = hdul2[i].data
                 max_val = np.nanmax(input_data)
+                min_val = np.nanmin(input_data)
+                fltr = np.isnan(input_data)
+                input_data[fltr] = max_val
+
+                input_data = misc.sample_image(input_data, 0.25)
+
                 fltr = input_data > .9*max_val
+                input_data[fltr] = np.nan
+                fltr = input_data < .9*min_val
                 input_data[fltr] = np.nan
 
                 break
@@ -53,18 +62,19 @@ for file in all_files[::24]:
                 row = 0
                 col = 0
                 for j in range(hdul[i].data.shape[0]):
-                    test_plot.colormap(hdul[i].data[j, :, :].T, ax_index=[row, col], show_colorbar=True)
+                    test_plot.colormap(hdul[i].data[j, ::-1, ::-1], ax_index=[row, col], show_colorbar=True)
                     col += 1
                     if col == n_cols:
                         col = 0
                         row += 1
                 for col in range(n_cols):
-                    test_plot.colormap(input_data, ax_index=[2, col], cmap_name="bwr", show_colorbar=True)
+                    test_plot.colormap(input_data.T, ax_index=[2, col], cmap_name="bwr", show_colorbar=True, vmin=-500, vmax=500)
                 test_plot.save(f"stats_{file}.png")
                 test_plot.close()
         hdul.close()
         hdul2.close()
-    except:
+    except Exception as e:
+        print(e)
         pass
             
 mean = stats/n
@@ -73,8 +83,8 @@ std = np.sqrt(sq_mean - mean**2)
 
 for i in range(stats.shape[0]):
     test_plot = plot.plot(nrows=1, ncols=2, size=plot.default_size(stats.shape[1]*10, stats.shape[2]*10))
-    test_plot.colormap(mean[i, :, :].T, ax_index=0, show_colorbar=True)
-    test_plot.colormap(std[i, :, :].T, ax_index=1, show_colorbar=True)
+    test_plot.colormap(mean[i, ::-1, ::-1], ax_index=0, show_colorbar=True)
+    test_plot.colormap(std[i, ::-1, ::-1], ax_index=1, show_colorbar=True)
     test_plot.save(f"stats{i}.png")
     test_plot.close()
 
