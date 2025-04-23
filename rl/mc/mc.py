6ec52e6692f82b1b8b63a1ee7fad0f0d8d74f132
nigulo <nolspert@gmail.com>
2025-04-23 23:10:30 +0300

    changes

---
+++ rl/mc/mc.py
@@ -0,0 +1,88 @@
+import numpy as np
+import numpy.random as random
+
+from typing import List, Dict, Set, Tuple, Callable
+import types
+
+class MC:
+    
+    #def __init__(self, 
+    #             actions: Callable[int, Set[int]], 
+    #             transitions: Callable[[int, int], List[Tuple[int, float, float]]]], 
+    #             b: Callable[int, List[Tuple[int, float]]]], 
+    #             s0: Callable[[], int], 
+    def __init__(self, actions, transitions, b, s0, state=None):
+        self.actions = actions
+        self.transitions = transitions
+        self.b = b
+        self.s0 = s0
+        self.state = state
+        
+    def train(self, gamma=0.9, n_episodes=1000, eps=1e-7, random_initial_policy=False):
+        q: Dict[int, Dict[int, float]] = {}        
+        pi: Dict[int, int] = {}
+        c: Dict[Tuple[int, int], float] = {}
+        episode = 0
+        if self.state:
+            q, pi, c, episode = self.state
+
+        for _ in range(episode, episode + n_episodes):
+            states = []
+            ps = []
+            rs = []
+            as_ = []
+            s = self.s0()
+            while True:
+                states.append(s)
+                a, p = self.random_action(s)                
+                s_r = self.random_transition(s, a)
+                if not s_r:
+                    break
+                s, r = s_r
+                ps.append(p)
+                rs.append(r)
+                as_.append(a)
+            
+            g = 0
+            w = 1
+            for t, s in enumerate(states[::-1]):
+                g += gamma*g + rs[t]
+                c_s_a = c.get((s, a), 0) + w
+                c[(s, a)] = c_s_a
+                q_s = q.get(s, {})
+                q_s_a = q_s.get(a, 0)
+                q_s_a += w/c_s_a*(g - q_s_a)
+                q_s[a] = q_s_a
+                q[s] = q_s
+                a, w_s = list(zip(*q_s.items()))
+                a_max = a[np.argmax(w_s)]
+                pi[s] = a_max
+                if a_max != as_[t]:
+                    break
+                w /= ps[t]
+                
+        self.state = q, pi, c, n_episodes
+        return q, pi
+    
+    def random_action(self, s):
+        a, p = list(zip(*self.b(s)))
+        ind = np.argmax(np.cumsum(p) >= random.random())
+        return a[ind], p[ind]
+    
+    def random_transition(self, s, a):
+        s_r_p = self.transitions(s, a)
+        if not s_r_p:
+            return None
+        s, r, p = list(zip(*s_r_p))
+        ind = np.argmax(np.cumsum(p) >= random.random())
+        return s[ind], r[ind]
+
+    def get_state(self):
+        return self.state
+    
+    def get_result(self):
+        q = {}        
+        pi = {}
+        if self.state:
+            q, pi, _, _ = self.state
+        return q, pi
---
+++ rl/mc/race_track.py
@@ -0,0 +1,85 @@
+import numpy as np
+import numpy.random as random
+import pickle
+import os
+from tqdm import tqdm
+import argparse
+
+from mc import MC
+
+def load(name="state"):
+    file = f"{name}.pkl"
+    if os.path.isfile(file):
+        return pickle.load(open(file, "rb"))
+    else:
+        return None
+
+def save(data, name="state"):
+    with open(f"{name}.pkl", "wb") as f:
+        pickle.dump(data, f)
+        
+class RaceTrack:
+
+    def __init__(self, track):
+        self.track = track
+        xs, ys = np.where(track == 2)
+        self.start = np.column_stack((xs, ys))
+
+    def on_track(self, x, y):
+        return 0 <= x < self.track.shape[0] and 0 <= y < self.track.shape[1] and self.track[x, y]
+    
+    def actions(self, s):
+        x, y, v_x, v_y = s
+        actions = set()
+        for a_x in [-1, 0, 1]:
+            if 0 <= v_x + a_x <= 4:    
+                for a_y in [-1, 0, 1]:
+                    if 0 <= v_y + a_y <= 4:
+                        actions.add((a_x, a_y))
+        return actions
+
+    def transitions(self, s, a):
+        x, y, v_x, v_y = s
+        if self.on_track(x, y):
+            if self.track[x, y] == 3:
+                return []
+            a_x, a_y = a
+            return [((x + v_x, y + v_y, v_x + a_x, v_y + a_y), -1, 1)]
+        x0, y0, v_x0, v_y0 = self.s0()
+        return [((x0, y0, v_x0, v_y0), -1, 1)]
+    
+    def b(self, s):
+        actions = self.actions(s)
+        p = 1/len(actions)
+        return [(a, p) for a in actions]
+
+    def s0(self):
+        x0, y0 = self.start[random.choice(len(self.start))]
+        return x0, y0, 0, 0
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--train', default=False, action='store_true')
+    args = parser.parse_args()
+    
+    track = np.genfromtxt("track1.txt", dtype=int, delimiter=1)[::-1]
+    
+    rt = RaceTrack(track)
+    mc = MC(rt.actions, rt.transitions, rt.b, rt.s0, state=load())
+    if args.train:
+        for _ in tqdm(range(10), desc="Training"):
+            mc.train(gamma=1, n_episodes=100)
+            save(mc.get_state())
+    q, pi = mc.get_result()
+    
+    s = rt.s0()
+    result = np.array(track)
+    while True:
+        result[s[0], s[1]] = 4
+        s_r = mc.random_transition(s, pi.get(s, mc.random_action(s)[0]))
+        if not s_r:
+            break
+        s, r = s_r
+        if not rt.on_track(s[0], s[1]):
+            break
+    print(result)
